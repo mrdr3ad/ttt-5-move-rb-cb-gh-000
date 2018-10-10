546b65070bd40d21f83ffd9b6fab50fcbe1512cb
Mwiza Chuba <byvuegfx@gmail.com>
2018-10-10 22:29:49 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -24,15 +24,10 @@ end
 
 
 def move (board,index, default = "X")
-  board[index]
+  board[index] = value 
 end
 
-board = [" ", " ", " "]
-def update_array_at_with(array, index, value)
-  array[index] = value
-end
 
-update_array_at_with(board, 0, "X")
 
 
 # code your input_to_index and move method here!
