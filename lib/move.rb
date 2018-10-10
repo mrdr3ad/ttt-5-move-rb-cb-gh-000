4a73ca88462d380417964dd6fa111931e8ffcc79
Mwiza Chuba <byvuegfx@gmail.com>
2018-10-10 22:18:44 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,6 @@
+
+board = [" "," "," "," "," "," "," "," "," "]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,5 +8,31 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+
+
+puts "Where would you like to go?"
+
+user_input = gets.strip
+
+
+def input_to_index (user_input)
+  converted_input = user_input.to_i
+  index = converted_input - 1
+end
+
+
+
+def move (board,index, default = "X")
+  board[index]
+end
+
+board = [" ", " ", " "]
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+
+update_array_at_with(board, 0, "X")
+
 
 # code your input_to_index and move method here!
